7ab5c7c692c18dbfb3bfdd1c33afda4864a8b806
thiagodinho <thiagodinhoan@gmail.com>
2023-03-23 15:24:00 -0300

    adiciona um exemplo prático do operador ternário

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores{
+    public static void main(String[] args) throws Exception {
+       
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String result = a==b ? "verdadeiro" : "falso"; //operador ternário
+
+        System.out.println(result);
+    }
+}
