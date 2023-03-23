283b780d6edcb2542c29d1e661fc5420d38c9188
thiagodinho <thiagodinhoan@gmail.com>
2023-03-23 15:55:31 -0300

    organiza os arquivos e packages

---
+++ Metodos/Metodos.java
@@ -0,0 +1,14 @@
+public class Metodos {
+    /*nomenclaura
+     * 
+     * soma(int n1, int n2){}
+     * 
+     * abrirConexao(){}
+     * 
+     * calcularMedia(){}
+     */
+
+     
+
+    
+}
---
+++ sistema-smart-tv/SmartTV.java
@@ -0,0 +1,5 @@
+//exercício prático do que foi mostrado até agora
+
+public class SmartTV {
+    
+}
